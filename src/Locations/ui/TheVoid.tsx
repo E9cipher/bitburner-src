39c2b5aec7b809ebea4c57134afc73696106c787
Enric <enricgalerarotger@gmail.com>
2026-06-07 12:35:37 +0200

    Create TheVoid.tsx, put basic contents

---
+++ src/Locations/ui/TheVoid.tsx
@@ -0,0 +1,11 @@
+/**
+ * React Subcomponent for displaying the void's UI
+ *
+ * This subcomponent renders all the void's content
+ */
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+import { Location } from "../Location";
+
+import { Player } from "@player";
