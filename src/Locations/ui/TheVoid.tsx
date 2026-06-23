f6a9ca5eefd898505637f07420e6a30a1fbd5153
Enric <enricgalerarotger@gmail.com>
2026-06-23 12:04:12 +0200

    Void content is empty now

--- src/Locations/ui/TheVoid.tsx
+++ src/Locations/ui/TheVoid.tsx
@@ -1,8 +1,6 @@
 import { Player } from "@player";
 import React, { useEffect } from "react";
 import { Exploit } from "../../Exploits/Exploit";
-import { Typography } from "@mui/material";
-import { CorruptibleText } from "../../ui/React/CorruptibleText";
 import { Page } from "../../ui/Router";
 import { Router } from "../../ui/GameRoot";
 export function TheVoid(): React.ReactElement {
@@ -10,10 +8,9 @@ export function TheVoid(): React.ReactElement {
         Player.giveExploit(Exploit.TheVoid);
         const baseDelay = 5000; // 5s
         setTimeout(() => Router.toPage(Page.City), (baseDelay + (baseDelay * Math.random()))); // baseDelay plus up to another baseDelay of random extra time
-    }, [])
-    return (
-        <Typography>
+    }, []);
+    /* <Typography>
             <CorruptibleText content={"You stand before what seems to be... nothing? A shiver runs down your spine and transports you back to where you belong."} spoiler={false} />
-        </Typography>
-    )
+        </Typography> */
+    return <></>;
 }
